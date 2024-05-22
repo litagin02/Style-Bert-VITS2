abdf7abdfbbb70eeae7e3b27fce53f70828541a4
will-castalk <will@castalk.com>
2024-05-22 15:19:39 +0700

    fix

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose-gpu.yml up -d --build && docker logs -f style-bert-vits2-backend-1
+docker-compose -f docker-compose-gpu.yml  up -d --build && docker logs -f style-bert-vits2-backend-1
